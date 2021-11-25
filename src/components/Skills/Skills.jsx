3f02963c21effbb882f22b2890deacad586828b1
Leslie Mares <lesliemares@Leslies-MacBook-Pro.local>
2021-11-24 23:10:30 -0800

    about me section update

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const Skills = () => {
+    return (
+        <Container className='Skills' fluid>
+
+        </Container>
+    )
+}
+
+export default Skills
